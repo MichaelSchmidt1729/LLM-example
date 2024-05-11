cf9e85389c812996dcc039a6817a403c105d9c8e
Michael Schmidt <Zweite.tontaube@gmail.com>
2024-05-11 23:35:50 +0200

    easy

--- first.py
+++ first.py
@@ -1,11 +1,35 @@
-import os
+
 from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.output_parsers.json import SimpleJsonOutputParser
+
+import os
 from dotenv import load_dotenv
 load_dotenv()
 openai_api_key = os.getenv('OPENAI_API_KEY')
 
-llm = OpenAI(openai_api_key=openai_api_key)
+llm = OpenAI(openai_api_key=openai_api_key,
+             model="gpt-3.5-turbo-instruct",
+             temperature=0
+             )
 
-response = llm.invoke("What is Neo4j?")
+template = PromptTemplate.from_template("""
+You are a cockney fruit and vegetable seller.
+Your role is to assist your customer with their fruit and vegetable needs.
+Respond using cockney rhyming slang.
+
+Output JSON as {{"description": "your response here"}}
+
+Tell me about the following fruit: {fruit}
+""")
+
+llm_chain = LLMChain(
+    llm=llm,
+    prompt=template,
+    output_parser=SimpleJsonOutputParser()
+)
+
+response = llm_chain.invoke({"fruit": "apple"})
 
 print(response)
